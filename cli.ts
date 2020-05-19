f2e6c91dc8a6b99ce15e1fc19acce97bc809d569
Avi <avidrucker@gmail.com>
2020-05-19 08:37:46 -0400

    feat show function

--- cli.ts
+++ cli.ts
@@ -14,8 +14,7 @@ const EAST: Vector  = { x: 1, y: 0 };
 const WEST: Vector  = { x:-1, y: 0 };
 
 type GameState = {
-	cols: number,
-	rows: number,
+	table: Table,
 	moves: Vector[],
 	snake: Point[],
 	apple: Point
@@ -25,8 +24,7 @@ type Point = { x: number, y: number };
 
 // 1.1 we define the game state object here
 const initializeState = (): GameState => ({
-	cols: 20,
-	rows: 14,
+	table: { cols: 20, rows: 14 },
 	moves: [EAST],
 	snake: [],
 	apple: {x: 16, y: 2}
@@ -34,3 +32,30 @@ const initializeState = (): GameState => ({
 
 // 1.1 we invoke the function to create the state here
 let State: GameState = initializeState();
+
+// 1.2 now that we have some state to render
+// we need a function to display the board...
+// But, we don't yet have any Matrix functions.
+// Let's fix that:
+
+// this appears to be called "asConstant" or "the constant function" ... what is it for?
+const k = (x: number) => (y: number) => x;
+
+// this function appears to map a function over a list of x values
+const map = (f: any) => (xs: any[]) => xs.map(f);
+const pipe = (...fns: any[]) => (x: any) => [...fns].reduce((acc, f) => f(acc), x);
+const range = (n: number) => (m: number) => 
+	Array.apply(null, Array(m - n)).map((_, i) => n + i); // what does "_" mean here?
+const rep = (c: any) => (n: any) => map(k(c))(range(0)(n));
+
+type Table = { cols: number, rows: number };
+
+const Matrix = {
+	make: (table: Table) => rep(rep('.')(table.cols))(table.rows),
+	toString: (xsxs: [][]) => xsxs.map(xs => xs.join(' ')).join('\r\n'),
+	fromState: (state: GameState) => pipe(
+		Matrix.make(state.table)
+	)(state)
+} 
+
+const show = () => console.log('\x1Bc' + Matrix.toString(Matrix.fromState(State)));
