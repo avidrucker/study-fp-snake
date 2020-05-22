bcef03dbfe97ba8d4dd73acaca6e24acd6961406
Avi Drucker <avidrucker@gmail.com>
2020-05-22 08:58:12 -0400

    update export game core from cli

--- cli.ts
+++ cli.ts
@@ -1,22 +1,5 @@
 const base = require('./fp-util');
-
-// function hasKey<O>(obj: O, key: keyof any): key is keyof O {
-//   return key in obj
-// }
-
-// https://dev.to/kingdaro/indexing-objects-in-typescript-1cgi
-// function hasOwnProperty<O extends object, K extends PropertyKey>(
-//   obj: O,
-//   key: K,
-// ): obj is O & Record<K, unknown> {
-//   return Object.prototype.hasOwnProperty.call(obj, key);
-// }
-
-// Object.getOwnPropertyNames(base).map((p: any) => {
-// 	if(hasOwnProperty(global, p)) {
-// 		global[p] = base[p]
-// 	}
-// });
+const core = require('./game-core');
 
 // I define functions here that only matter for
 // input/output & interfacing with the terminal.
@@ -25,33 +8,8 @@ const base = require('./fp-util');
 // which requires only constants for the
 // directional vectors
 
-type Vector = { x: number, y: number };
-
-// Constants
-const NORTH: Vector = { x: 0, y:-1 };
-const SOUTH: Vector = { x: 0, y: 1 };
-const EAST: Vector  = { x: 1, y: 0 };
-const WEST: Vector  = { x:-1, y: 0 };
-
-type GameState = {
-	table: Table,
-	moves: Vector[],
-	snake: Point[],
-	apple: Point
-};
-
-type Point = { x: number, y: number };
-
-// 1.1 we define the game state object here
-const initializeState = (): GameState => ({
-	table: { cols: 20, rows: 14 },
-	moves: [EAST],
-	snake: [],
-	apple: {x: 16, y: 2}
-});
-
 // 1.1 we invoke the function to create the state here
-let State: GameState = initializeState();
+let State: GameState = core.initializeState();
 
 // 1.2 now that we have some state to render
 // we need a function to display the board...
@@ -60,7 +18,6 @@ let State: GameState = initializeState();
 
 // this appears to be called "asConstant" or "the constant function" ... what is it for?
 // is this relevant? https://www.maplesoft.com/support/help/Maple/view.aspx?path=MathApps/ConstantFunction
-type Table = { cols: number, rows: number };
 
 const Matrix = {
 	make: (table: Table): Table => base.rep(base.rep('.')(table.cols))(table.rows),
--- game-core.ts
+++ game-core.ts
@@ -1,3 +1,33 @@
 // I define here the core snake game logic, that
 // only concerns itself with how the game works,
 // not with presentation, nor with input/output.
+
+const base = require('./fp-util');
+
+type Vector = { x: number, y: number };
+type Point = { x: number, y: number };
+type Table = { cols: number, rows: number };
+
+// Constants
+const NORTH: Vector = { x: 0, y:-1 };
+const SOUTH: Vector = { x: 0, y: 1 };
+const EAST: Vector  = { x: 1, y: 0 };
+const WEST: Vector  = { x:-1, y: 0 };
+
+type GameState = {
+	table: Table,
+	moves: Vector[],
+	snake: Point[],
+	apple: Point
+};
+
+// 1.1 we define the game state object here
+const initializeState = (): GameState => ({
+	table: { cols: 20, rows: 14 },
+	moves: [EAST],
+	snake: [],
+	apple: {x: 16, y: 2}
+});
+
+// enqueue, next,
+module.exports = { EAST, NORTH, SOUTH, WEST, initializeState }
