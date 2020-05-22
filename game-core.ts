c295b80c29431a7b8dc918f315414cf4b876f229
Avi Drucker <avidrucker@gmail.com>
2020-05-22 09:17:58 -0400

    update explicitly export types, comment on set()

--- game-core.ts
+++ game-core.ts
@@ -4,9 +4,9 @@
 
 const base = require('./fp-util');
 
-type Vector = { x: number, y: number };
-type Point = { x: number, y: number };
-type Table = { cols: number, rows: number };
+export type Vector = { x: number, y: number };
+export type Point = { x: number, y: number };
+export type Table = { cols: number, rows: number };
 
 // Constants
 const NORTH: Vector = { x: 0, y:-1 };
@@ -14,7 +14,7 @@ const SOUTH: Vector = { x: 0, y: 1 };
 const EAST: Vector  = { x: 1, y: 0 };
 const WEST: Vector  = { x:-1, y: 0 };
 
-type GameState = {
+export type GameState = {
 	table: Table,
 	moves: Vector[],
 	snake: Point[],
@@ -38,6 +38,9 @@ const initializeState = (): GameState => ({
 // function result with the state passed in as a parameter.
 // So, addSnake() is likely on the add list. I stub this now
 // inside of the Matrix function (table map) object.
+
+// 2.11 In order to addSnake() we need to create the base set()
+// because it is called inside of addSnake()
 
 // enqueue, next,
 module.exports = { EAST, NORTH, SOUTH, WEST, initializeState }
